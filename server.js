dbd50743f13a712b5865029f26899911625a017d
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2020-06-29 21:17:34 +0200

    db connection

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./routes/api/items');
+
 const app = express();
 
 // Bodyparser Middleware
@@ -14,6 +16,10 @@ const db = require('./config/keys').mongoURI;
 mongoose.connect(db)
 .then(() => console.log('A MongoDB csatlakoztatva van.'))
 .catch(err => console.log(err));
+
+// Use Routes
+const mainRoutes = require('./Routes/api/items')
+app.use(mainRoutes);
 
 const port = process.env.PORT || 5000;
 
